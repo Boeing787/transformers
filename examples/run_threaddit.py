875de10acf934390d168e76e30921be3e56f9d63
Joseph Hall <jhallpilot@gmail.com>
2020-05-09 16:30:32 -0700

    add new threddit data processor

--- examples/run_threaddit.py
+++ examples/run_threaddit.py
@@ -15,6 +15,7 @@ logger = logging.getLogger(__name__)
 @dataclass
 class ThredditInputExample(object):
     """A single training/test example for the Threddit dataset."""
+    """We replicate the parent four times so that the model sees how each parent is related to the ith child comment"""
 
     def __init__(self, example_id, response, parent, child_comment_0, child_comment_1, child_comment_2, child_comment_3, label=None):
         self.example_id = example_id
@@ -46,3 +47,68 @@ class ThredditInputExample(object):
             attributes.append("label: {}".format(self.label))
 
         return ", ".join(attributes)
+
+
+class ThredditProcessor(DataProcessor):
+    """Processor for the Reddit analysis project."""
+
+    def get_example_from_tensor_dict(self, tensor_dict):
+        example_id = tensor_dict["idx"].numpy()
+        response = tensor_dict["response"].numpy()
+        parent = tensor_dict["parent"].numpy().decode("utf-8")
+        label = str(tensor_dict["label"].numpy())
+
+        return ThredditInputExample(
+            example_id,
+            response,
+            parent,
+            tendor_dict["child_comment_0"].numpy().decode("utf-8"),
+            tensor_dict["child_comment_1"].numpy().decode("utf-8"),
+            tensor_dict["child_comment_2"].numpy().decode("utf-8"),
+            tensor_dict["child_comment_3"].numpy().decode("utf-8"),
+            label
+        )
+
+    def get_train_examples(self, data_dir):
+        logger.info("LOOKING AT {}".format(os.path.join(data_dir, "train.jsonl"))) 
+        with open(os.path.join(data_dir, "train.jsonl"), "r") as f:
+            return self._create_examples(f.read().splitlines(), "train")
+
+    def get_dev_examples(self, data_dir):
+        """See base class."""
+        with open(os.path.join(data_dir, "val.jsonl"), "r") as f:
+            return self._create_examples(f.read().splitlines(), "val")
+
+    def get_labels(self):
+        return ["0", "1", "2", "3"]
+
+    def _create_examples(self, lines, set_type):
+        examples = []
+        for (i, line) in enumerate(lines):
+            if i == 0:
+                continue
+            line = loads(line)
+            guid = "%s-%s" % (set_type, i)
+            try:
+                response = line["response"]
+                parent = line["parent"]
+                cc0 = line["child_comment_0"]
+                cc1 = line["child_comment_1"]
+                cc2 = line["child_comment_2"]
+                cc3 = line["child_comment_3"]
+                label = line["label"]
+            except IndexError:
+                continue
+            examples.append(
+                ThredditInputExample(
+                    guid,
+                    response,
+                    parent,
+                    cc0,
+                    cc1,
+                    cc2,
+                    cc3,
+                    label
+                )
+            )
+        return examples
